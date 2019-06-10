97cfeeef3994e44965d0ca782881d3bc9b30eedd
Ellen Covey <covey.ellen@gmail.com>
2019-06-10 12:42:35 +0100

    Initial Commit

---
+++ Curious_Katie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Curious_Katie
+//
+//  Created by Ellen Covey on 10/06/2019.
+//  Copyright © 2019 Ellen Covey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
